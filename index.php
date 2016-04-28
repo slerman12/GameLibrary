a13999d48af9a1ad1845fd7cdb2bfb361b4311a4
Sam Lerman <slerman@u.rochester.edu>
2016-04-28 07:16:16 -0400

    Created homepage

--- index.php
+++ index.php
@@ -11,8 +11,72 @@ catch (PDOException $e) {
     die();
 }
 ?> <html>
-   <head><title>DB Demo</title></head>
-   <body>
-      <p>There are <?= $cnt ?> tuples in the Wegmans ITEM table.</p>
-   </body>
+<head>
+    <title>DB Demo</title>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <!-- BOOTSTRAP Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- BOOTSTRAP Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- BOOTSTRAP Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <link href="styles.css" rel="stylesheet">
+</head>
+<body>
+<div class="gamelibrary-masthead">
+    <div class="container">
+        <nav class="gamelibrary-nav">
+            <a class="gamelibrary-nav-item active" href="#">Home</a>
+            <div class="pull-right">
+                <a class="gamelibrary-nav-item" href="#">Sam's List</a>
+                <a class="gamelibrary-nav-item" href="#">Cinthia's List</a>
+                <a class="gamelibrary-nav-item" href="#">Rodrigo's List</a>
+                <a class="gamelibrary-nav-item" href="#">Lee's List</a>
+            </div>
+        </nav>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 gamelibrary-main">
+
+            <div class="gamelibrary-post">
+                <h2 class="gamelibrary-post-title">Game Library</h2>
+                <hr>
+                <p>Gamelibrary description goes here. For best appearance, make it three paragraphs as shown here with this placeholder text. A few sentences per paragraph should be good, at around this length.</p>
+                <hr>
+                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                <hr>
+                <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div><!-- /.gamelibrary-post -->
+
+        </div><!-- /.gamelibrary-main -->
+
+        <div class="col-sm-3 col-sm-offset-1 gamelibrary-sidebar">
+            <div class="sidebar-module">
+                <h4>Lists</h4>
+                <ol class="list-unstyled">
+                    <li><a href="#">Sam - RelationName</a></li>
+                    <li><a href="#">Cinthia - RelationName</a></li>
+                    <li><a href="#">Rodrigo - RelationName</a></li>
+                    <li><a href="#">Lee - RelationName</a></li>
+                </ol>
+            </div>
+        </div><!-- /.gamelibrary-sidebar -->
+
+    </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+<footer class="gamelibrary-footer">
+    <p><a href="#">Project Design Document</a>.</p>
+</footer>
+
+</div>
+</body>
 </html>
