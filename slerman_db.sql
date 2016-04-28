477c99bec15272f98fe5e89681fa07ab5f88024e
Sam Lerman <slerman@u.rochester.edu>
2016-04-28 04:59:47 -0400

    Calibrated slerman_db across local and remote

---
+++ slerman_db.sql
@@ -0,0 +1,79 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.2
+-- Dumped by pg_dump version 9.5.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: items; Type: TABLE; Schema: public; Owner: slerman
+--
+
+CREATE TABLE items (
+    name character varying(10) NOT NULL
+);
+
+
+ALTER TABLE items OWNER TO slerman;
+
+--
+-- Data for Name: items; Type: TABLE DATA; Schema: public; Owner: slerman
+--
+
+COPY items (name) FROM stdin;
+one
+two
+three
+\.
+
+
+--
+-- Name: items_pkey; Type: CONSTRAINT; Schema: public; Owner: slerman
+--
+
+ALTER TABLE ONLY items
+    ADD CONSTRAINT items_pkey PRIMARY KEY (name);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
