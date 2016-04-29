4ee6b307fe76f449ea6a82945b15238dc3f266fb
Sam Lerman <slerman@u.rochester.edu>
2016-04-28 20:13:25 -0400

    Finished lists skeleton

--- list.php
+++ list.php
@@ -2,13 +2,36 @@
 // File: list.php
 // Purpose: lists
 require_once ('./dbsetup.php');
-try {
-    $rslt = $db->query('SELECT COUNT(*) FROM items');
-    $cnt = $rslt ? $rslt->fetchColumn() : -1;
-}
-catch (PDOException $e) {
-    print "DB Query Error : " . $e->getMessage();
-    die();
+
+//create HTML table from a psql query ($table) with two attributes of your choice as columns
+function createList($table, $attribute1, $attribute2){
+
+    echo    '<div class="table-responsive list-table">',
+                '<table class="table table-striped table-hover">',
+                    '<thead>',
+                    '<tr>',
+                        '<th></th>',
+                        '<th>',ucfirst($attribute1),'</th>',
+                        '<th>', ucfirst($attribute2),'</th>',
+                        '<th></th>',
+                        '<th></th>',
+                    '</tr>',
+                    '</thead>',
+                    '<tbody>';
+    $i = 0;
+    while (($row = $table->fetch()) && ($i < 15)) {
+        echo        '<tr>',
+                        '<td><a href="#"><i class="fa fa-times text-danger"></i></a></td>',
+                        '<td>', $row->$attribute1, '</td>',
+                        '<td>', $row->$attribute2, '</td>',
+                        '<td><button type="button" class="btn btn-info btn-sm">Relationships</button></td>',
+                        '<td><button type="button" class="btn btn-primary btn-sm">Details/Edit</button></td>',
+                    '</tr>';
+        $i++;
+    }
+    echo            '</tbody>',
+                '</table>',
+            '</div>';
 }
 ?> <html>
 <head>
@@ -28,10 +51,10 @@ catch (PDOException $e) {
         <nav class="gamelibrary-nav">
             <a class="gamelibrary-nav-item active" href="./index.php">Game <i style="font-size: 18px;" class="fa fa-gamepad" aria-hidden="true"></i> Library  </a>
             <div class="pull-right">
-                <a class="gamelibrary-nav-item" href="./list.php?who=Sam">Sam's List</a>
-                <a class="gamelibrary-nav-item" href="./list.php?who=Cinthia">Cinthia's List</a>
-                <a class="gamelibrary-nav-item" href="./list.php?who=Rodrigo">Rodrigo's List</a>
-                <a class="gamelibrary-nav-item" href="./list.php?who=Lee">Lee's List</a>
+                <a class="gamelibrary-nav-item" href="./list.php?Sam">Sam's List</a>
+                <a class="gamelibrary-nav-item" href="./list.php?Cinthia">Cinthia's List</a>
+                <a class="gamelibrary-nav-item" href="./list.php?Rodrigo">Rodrigo's List</a>
+                <a class="gamelibrary-nav-item" href="./list.php?Lee">Lee's List</a>
             </div>
         </nav>
     </div>
@@ -41,8 +64,51 @@ catch (PDOException $e) {
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2 list-main">
             <button class="insert-button btn btn-success"><i class="fa fa-plus"></i></button>
-            <h2>Platforms</h2>
-            <p>Search for platforms, view related entities, edit, or show details. Use the plus button to insert, the red X button to delete.</p>
+            <?php
+
+            // Select table query and display list
+            try {
+                $who = $_SERVER['QUERY_STRING'];
+                if ($who == 'Sam'){
+                    $table = $db->query('SELECT name, version, type, speed, popularity FROM platforms', PDO::FETCH_OBJ);
+                    $attribute1 = 'name';
+                    $attribute2 = 'version';
+                    $listName = 'Platforms';
+                    $listDesc = 'Search for platforms, view related entities, edit, or show details. Use the plus button to insert, the red X button to delete.';
+                }
+                elseif ($who == 'Cinthia'){
+                    $table = $db->query('');
+                    $attribute1 = '';
+                    $attribute2 = '';
+                    $listName = '';
+                    $listDesc = '';
+                }
+                elseif ($who == 'Rodrigo'){
+                    $table = $db->query('');
+                    $attribute1 = '';
+                    $attribute2 = '';
+                    $listName = '';
+                    $listDesc = '';
+                }
+                elseif ($who == 'Lee'){
+                    $table = $db->query('');
+                    $attribute1 = '';
+                    $attribute2 = '';
+                    $listName = '';
+                    $listDesc = '';
+                }
+                else{
+
+                }
+            }
+            catch (PDOException $e) {
+                print "DB Query Error : " . $e->getMessage();
+                die();
+            }
+
+            ?>
+            <h2><?php echo $listName; ?></h2>
+            <p><?php echo $listDesc; ?></p>
             <div id="custom-search-input">
                 <div class="input-group col-md-12">
                     <input type="text" class="  search-query form-control" placeholder="Search" />
@@ -53,42 +119,18 @@ catch (PDOException $e) {
                                 </span>
                 </div>
             </div>
-            <div class="table-responsive list-table">
-                <table class="table table-striped table-hover">
-                    <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Version</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td><a href="#"><i class="fa fa-times text-danger"></i></a></td>
-                        <td>Windows</td>
-                        <td>2.0</td>
-                        <td><button type="button" class="btn btn-info btn-sm">Relationships</button></td>
-                        <td><button type="button" class="btn btn-primary btn-sm">Details/Edit</button></td>
-                    </tr>
-                    <tr>
-                        <td><a href="#"><i class="fa fa-times text-danger"></i></a></td>
-                        <td>iOS</td>
-                        <td>1.001</td>
-                        <td><button type="button" class="btn btn-info btn-sm">Relationships</button></td>
-                        <td><button type="button" class="btn btn-primary btn-sm">Details/Edit</button></td>
-                    </tr>
-                    <tr>
-                        <td><a href="#"><i class="fa fa-times text-danger"></i></a></td>
-                        <td>Linux Debian</td>
-                        <td>10</td>
-                        <td><button type="button" class="btn btn-info btn-sm">Relationships</button></td>
-                        <td><button type="button" class="btn btn-primary btn-sm">Details/Edit</button></td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+            <?php
+
+                // Select table query and display list
+                try {
+                    createList($table, $attribute1, $attribute2);
+                }
+                catch (PDOException $e) {
+                    print "DB Query Error : " . $e->getMessage();
+                    die();
+                }
+
+            ;?>
         </div>
 
     </div><!-- /.row -->
